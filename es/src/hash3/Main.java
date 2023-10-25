f49796e3df78307b269af715037265bf93eab9e3
LucaSavini <145345180+LucaSavini@users.noreply.github.com>
2023-10-25 16:46:19 +0200

    Add files via upload

---
+++ es/src/hash3/Main.java
@@ -0,0 +1,49 @@
+package hash3;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<String> words = new HashSet<String>();
+        String stringa = new String("Luca");
+
+        System.out.println("Set originale : " );
+        fillSet(words);
+        System.out.println("Set aggiornato : " + checkSet(words,stringa));
+        clearSet(words);
+    }
+
+    public static HashSet<String> fillSet(HashSet<String> words){
+        words.add("Pietro");
+        words.add("Riccardo");
+        words.add("Andrea");
+        words.add("Marco");
+        words.add("Luca");
+        return words;
+    }
+
+    public static HashSet<String> checkSet (HashSet<String> words, String stringa){
+        Iterator<String> iterator = words.iterator();
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+            if (stringa.equals(element)){
+                iterator.remove();
+            }
+        }
+        return words;
+    }
+
+    public static  HashSet<String> clearSet(HashSet<String> words){
+        words.clear();
+        if(words.isEmpty()){
+            System.out.println("L'Hashet è vuoto");
+        } else {
+            System.out.println("L'Hashset è pieno");
+        }
+        return words;
+    }
+
+
+}
